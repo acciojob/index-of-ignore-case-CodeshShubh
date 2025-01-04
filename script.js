fed3646cdd9cad286f3efb0432f9e462c9afae1d
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-04 07:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+  let lowerS1 = s1.toLowerCase();
+  let lowerS2 = s2.toLowerCase();
+     // find the index of s2 in s1
+  let firstOccurrence = lowerS1.indexOf(lowerS2);
+
+  // return the index
+  return firstOccurrence;
+	
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+alert(indexOfIgnoreCase(s1, s2)); 
